b8ba285d8b1479c0416366473b1c38185ff1633b
mrfakename <me@mrfake.name>
2023-12-30 12:02:56 -0800

    New features! & a license!

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
 
 # HFHubPHP Alpha
 
-A PHP library for interacting with the Hugging Face Hub. Everything is done in a single file, so
+*A portable (in both senses, easily installable and easy to port), fast, and simple wrapper for the Hugging Face Hub APIs, similar to the HF Hub Python library.*
+
+A PHP library for interacting with the Hugging Face Hub. Everything is done in a single file, so all you have to do is drag the file in and import it with `include`!
+
+**This library is designed to be *portable*, *simple*, and *easily portable* to another language.** If you're interested in porting this library to another language, please see the Porting section below!
 
 ## Todo
 
+We aim to support the following methods, with similar APIs as the original `huggingface_hub` Python library:
+
 - [ ] Inference APIs
-- [ ] User
+- [x] User
   - [x] Whoami
-- [ ] Repo Operations
+- [x] Repo Operations
+    - [x] Full HF Spaces support
   - [x] Create
-    - [ ] Full HF Spaces support
   - [x] Delete
   - [x] Move
   - [x] Update Visibility
@@ -19,11 +25,18 @@ A PHP library for interacting with the Hugging Face Hub. Everything is done in a
     - [x] Create
     - [x] Download
     - [x] Delete
-  - [ ] Folder
+    - [x] List
+  - [x] Folder
     - [x] Delete
   - [ ] Later: Add LFS support
 
 ## Documentation
+
+### Installation
+
+Just drag'n'drop the classes you need into your project folder! `hub.php` includes everything you need to interact with the HF Hub!
+
+### Usage
 
 More documentation coming soon!
 
@@ -32,25 +45,41 @@ include 'hub.php';
 $api = new Hub('hf_***');
 ```
 
-## Methods
+### Methods
+
+> NOTE: Starting with PHP 8, you can now use named arguments! This means you can call `$api->create_repo` as `$api->create_repo('username/model', private: True)`!
 
 ```php
-$api->create_repo($repo_id, $repo_type = 'model', $private = False);
+# API
+$api = new Hub($token)
+# Repo operations
+$api->create_repo($repo_id, $repo_type = 'model', $private = False, $space_sdk = null, $space_hardware = null, $space_storage = null, $space_sleep_time = null, $space_secrets = null, $space_variables = null)
 $api->delete_repo($repo_id, $repo_type = 'model')
 $api->move_repo($from_id, $to_id, $repo_type = 'model')
-
+# File operations
 $api->upload_string($repo_id, $path, $string, $commit_message, $commit_description = null, $revision = 'main', $repo_type = 'model')
 $api->upload_file($repo_id, $path, $file, $commit_message, $commit_description = null, $revision = 'main', $repo_type = 'model')
 $api->delete_file($repo_id, $path, $commit_message = "Delete File", $commit_description = null, $revision = 'main', $repo_type = 'model')
 $api->delete_folder($repo_id, $path, $commit_message = "Delete Folder", $commit_description = null, $revision = 'main', $repo_type = 'model')
-
 $api->download_file($repo_id, $file, $path, $repo_type = 'model', $revision = 'main')
-
-$api->update_repo_visibility($repo_id, $private, $repo_type = 'model');
-$api->
-whoami()
+# List operations
+$api->list_files($repo_id, $repo_path = '', $repo_type = 'model', $revision = 'main')
+# Update operations
+$api->update_repo_visibility($repo_id, $private, $repo_type = 'model')
+# User operations
+$api->whoami()
+# Inference APIs
+$api->generate($model_id, $inputs, $min_length = 0, $max_length = 256, $top_k	= null, $top_p = null, $temperature = 1.0, $repetition_penalty = null, $max_time = 120, $do_sample = null, $use_cache = false, $wait_for_model = true)
 ```
+
+## Porting
+
+This library is designed to be easy to port! Please open an Issue if you're interested in porting this library. If this library is ported to enough languages, it might be good to create a centralized GitHub organization.
+
+## Credits/Acknowledgements
+
+The entire library is basically a reimplementation of the [`huggingface_hub` library](https://github.com/huggingface/huggingface_hub) in PHP, with modifications to make it more portable. The API implementation is almost completely based on that library.
 
 ## License
 
-This software is currently not licensed. We plan to add a license soon.
+This software is licensed under the Mozilla Public License, version 2.0. The Mozilla Public License encourages sharing, however it does not have the same restrictions as the GNU General Public License, as it does *not* require linked software to be licensed under the same license. If you have any questions about the license, please open an Issue on GitHub.
--- src/hub.php
+++ src/hub.php
@@ -6,6 +6,7 @@ class Hub
     # Public Variables
     public string $useragent = 'mrfakename/hfhubphp; php/' . PHP_VERSION;
     public string $base_url = 'https://huggingface.co/';
+    public string $inference_endpoint = 'https://api-inference.huggingface.co/';
     # Internal Functions
     private function _create_headers($token = null)
     {
@@ -37,6 +38,35 @@ class Hub
         curl_close($ch);
         return $content;
     }
+    private function _cgc_headers($url)
+    {
+        $ch = curl_init();
+        $headers = [];
+        curl_setopt(
+            $ch,
+            CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $headers[strtolower(trim($header[0]))][] = trim($header[1]);
+                return $len;
+            }
+        );
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 25);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->_create_headers());
+        $content = curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new \Exception('cURL error: ' . curl_error($ch));
+        }
+        curl_close($ch);
+        return [$content, $headers];
+    }
     # Construct
     public function __construct($token)
     {
@@ -44,7 +74,7 @@ class Hub
     }
     # Public Functions
     # Repo Functions
-    public function create_repo($repo_id, $repo_type = 'model', $private = False)
+    public function create_repo($repo_id, $repo_type = 'model', $private = False, $space_sdk = null, $space_hardware = null, $space_storage = null, $space_sleep_time = null, $space_secrets = null, $space_variables = null)
     {
         $this->_validate_type($repo_type);
         list($repouser, $reponame) = explode('/', $repo_id, 2);
@@ -54,6 +84,17 @@ class Hub
             'type' => $repo_type,
             'private' => $private ? 'true' : 'false'
         ];
+        $json_keys = ["hardware", "storageTier", "sleepTimeSeconds", "secrets", "variables"];
+        $values = [$space_hardware, $space_storage, $space_sleep_time, $space_secrets, $space_variables];
+
+        if ($repo_type == 'space') {
+            foreach (array_filter(array_combine($json_keys, $values), function ($v) {
+                return $v !== null;
+            }) as $k => $v) {
+                $payload[$k] = $v;
+            }
+        }
+
         $body = $payload;
         $headers = $this->_create_headers();
         $ch = curl_init($this->base_url . 'api/repos/create');
@@ -258,5 +299,67 @@ class Hub
         curl_close($ch);
         fclose($file);
         return true;
+    }
+    # List Files
+    public function list_files($repo_id, $repo_path = '', $repo_type = 'model', $revision = 'main')
+    {
+        $this->_validate_type($repo_type);
+        $revision = rawurlencode($revision);
+        $url = $this->base_url . "api/$repo_type" . "s/" . $repo_id . "/tree/$revision/" . rawurlencode($repo_path);
+        list($res, $headers) = $this->_cgc_headers($url);
+        $currentRes = $res;
+        $result = json_decode($res, true);
+        # Pagination
+        // var_dump($headers);
+        while (isset($headers['link'])) {
+            # Can paginate
+            $url = $headers["link"][0];
+            $pattern = '/<([^>]+)>/';
+            preg_match($pattern, $url, $matches);
+            if (isset($matches[1])) {
+                $url = $matches[1];
+                list($res, $headers) = $this->_cgc_headers($url);
+                $currentRes = json_decode($res, true);
+                $result = array_merge($result, $currentRes);
+            }
+        }
+        return $result;
+    }
+    # Inference APIs
+    public function generate($model_id, $inputs, $min_length = 0, $max_length = 256, $top_k = null, $top_p = null, $temperature = 1.0, $repetition_penalty = null, $max_time = 120, $do_sample = null, $use_cache = false, $wait_for_model = true)
+    {
+        $url = $this->inference_endpoint . 'models/' . $model_id;
+        $headers = $this->_create_headers();
+        $ch = curl_init($url);
+        $parameters = [
+            'min_length' => $min_length,
+            'max_length' => $max_length,
+            'top_k' => $top_k,
+            'top_p' => $top_p,
+            'temperature' => $temperature,
+            'repetition_penalty' => $repetition_penalty,
+            'max_time' => $max_time,
+        ];
+        foreach ($parameters as $k => $v) {
+            if (!$v) {
+                unset($parameters[$k]);
+            }
+        }
+        $args = [
+            'inputs' => $inputs,
+            'options' => [
+                'use_cache' => $use_cache,
+                'wait_for_model' => $wait_for_model
+            ],
+            'parameters' => $parameters
+        ];
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($response, true);
     }
 }
